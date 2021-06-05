2ad3d6177c8bd9d99a2923c1a062101eaadc03de
Carlos_Santini <jusca_94@hotmail.com>
2021-06-05 15:35:42 -0500

    - a function to save the 'especie' from 'sirenas migrantes' was added.
    - Then, sirenas_endemicas_y_sirenas_migrantes.csv is updated by the 'especie'

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 import pandas as pd
 from src import config
 from feature_processing import feature_process
+from train import training
+
+
+def save_sirenas_migrantes(file, y_predicts):
+    # print(file.columns)
+    file['especie'] = y_predicts
+    file.to_csv(config.SIRENAS, index=False)
 
 
 if __name__ == '__main__':
@@ -11,5 +18,17 @@ if __name__ == '__main__':
     # Transformacion debida para los datasets
     X_train, y_train, X_test = feature_process(sirenas_historico, sirenas)
 
+    '''
+    "NearestCentroid"
+    "MLPClassifier"
+    "SGDClassifier"
+    "LogisticRegression"
+    "SVC1"
+    "SVC2"
+    "DecisionTree"
+    "RandomForest"
+    '''
 
+    y_test = training(X_train, y_train, X_test, 'NearestCentroid')
 
+    save_sirenas_migrantes(sirenas, y_test)
