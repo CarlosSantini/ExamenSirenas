f6803e156c0b90d5f4b348149bc193f6125f3d90
Carlos_Santini <jusca_94@hotmail.com>
2021-06-05 15:08:37 -0500

    - feature_processing was added. This function transforms the labels from string to int to used during training phase

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from feature_processing import feature_process
 
 
 if __name__ == '__main__':
+    # Leer los archivos csv originales
     sirenas_historico = pd.read_csv(config.SIRENAS_HISTORICO)
     sirenas = pd.read_csv(config.SIRENAS)
 
+    # Transformacion debida para los datasets
     feature_process(sirenas_historico, sirenas)
